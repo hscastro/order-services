7e8bcd4e75a44ac26a352c2a8907f5b3978296bb
hscastro <halisonsc5@gmail.com>
2022-07-07 00:19:36 -0300

    Refatoração das classes Pessoa e Tecnico

--- ordem-servico/src/main/java/com/hscastro/ordemservico/domain/Pessoa.java
+++ ordem-servico/src/main/java/com/hscastro/ordemservico/domain/Pessoa.java
@@ -2,6 +2,12 @@ package com.hscastro.ordemservico.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -15,12 +21,21 @@ import lombok.ToString;
 @EqualsAndHashCode
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public abstract class Pessoa implements Serializable {
 	
 	private static final long serialVersionUID = 8329493081197829546L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "nome", length = 255)
 	private String nome;
+
+	@Column(name = "cpf", length = 11)
 	private String cpf;
+	
+	@Column(name = "telefone", length = 20)
 	private String telefone;
 }
--- ordem-servico/src/main/java/com/hscastro/ordemservico/domain/Tecnico.java
+++ ordem-servico/src/main/java/com/hscastro/ordemservico/domain/Tecnico.java
@@ -1,9 +1,21 @@
 package com.hscastro.ordemservico.domain;
 
-import lombok.AllArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class Tecnico extends Pessoa {
 	
 	private static final long serialVersionUID = -7499195410060967724L;
+
+	private List<OrdemServico> lista = new ArrayList<>();
+
 }
